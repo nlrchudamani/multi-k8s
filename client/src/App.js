c9e6301cc0047cdf7469d84b65870786d4193a9d
N L Ramachandrachudamani <sagarmani1986@gmail.com>
2021-06-23 22:19:32 +0530

    testing ci cd

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
-          <Link to="/otherPage">Other Page </Link>
+          <Link to="/otherPage">Other Pages </Link>
 
          
         <a
